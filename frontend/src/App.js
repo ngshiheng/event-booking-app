9f9a1de56b7a835cf5a60d17fb6479936374e90c
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-09-26 21:49:52 +0800

    Front end first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <h1>It works!</h1>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
