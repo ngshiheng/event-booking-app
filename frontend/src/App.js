2b560f899f4e67c4bcc42b26c3806434d2385191
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-09-28 11:24:55 +0800

    Added basic css styling, auth page is now able to login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
 
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <h1>It works!</h1>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+
