bd9dc1a804f39ae22763d78f700a0f47a8586965
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-10-03 10:57:04 +0800

    Added BookingList component, user can now cancelBooking

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuthContext from '../context/auth-context';
 import Spinner from '../components/Spinner/Spinner';
+import BookingList from '../components/Bookings/BookingList/BookingList';
 
 class BookingsPage extends Component {
     state = {
@@ -57,20 +58,56 @@ class BookingsPage extends Component {
             });
     };
 
+    deleteBookingHandler = bookingId => {
+        this.setState({ isLoading: true });
+        const requestBody = {
+            query: `
+                mutation {
+                    cancelBooking(id: "${bookingId}") {
+                        id
+                        eventId {
+                            title
+                        }
+                    }
+                }
+            `
+        };
+        const token = this.context.token;
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 && res.status !== 201) {
+                    throw new Error('Failed');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                this.setState(prevState => {
+                    const updatedBookings = prevState.bookings.filter(booking => {
+                        return booking.id !== bookingId;
+                    });
+                    return { bookings: updatedBookings, isLoading: false };
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ isLoading: false });
+            });
+    }
+
     render() {
         return (
             <React.Fragment>
                 {this.state.isLoading ? (
                     <Spinner />
                 ) : (
-                        <ul>
-                            {this.state.bookings.map(booking => (
-                                <li key={booking.id}>
-                                    {booking.eventId.title} -{' '}
-                                    {new Date(booking.createdAt).toLocaleDateString()}
-                                </li>
-                            ))}
-                        </ul>
+                        <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />
                     )}
             </React.Fragment>
         );
