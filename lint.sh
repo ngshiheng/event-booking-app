57be5924ade7cc3d93ab457c39ed584272754f77
jerryng <Jerry Ng>
2019-11-10 10:19:13 +0800

    Update script to exit 1 on first error

--- lint.sh
+++ lint.sh
@@ -3,6 +3,7 @@
 # Run this before commiting any code to check for linting errors and formatting
 
 # font color
+red=`tput setaf 1`
 green=`tput setaf 2`
 white=`tput sgr0`
 
@@ -11,7 +12,22 @@ echo_finish () {
     echo "$green✓$white Finished checking."
 }
 
+echo_ok() {
+    echo "$green✓$white $1"
+}
+
+echo_error() {
+    echo "$red✗$white $1"
+}
+
+lint_check() {
+    echo "⌛ $white Running eslint & prettier at $1"
+    npm run lint --prefix $1
+    if [ "$?" -ne "0" ]; then echo_error "ERR - error while running eslint/prettier $1" && exit 1; fi
+    echo_ok "OK - no eslint/prettier errors found at $1"
+}
+
 # main
-npm run lint --prefix ./server
-npm run lint --prefix ./client
+lint_check ./server
+lint_check ./client
 echo_finish
